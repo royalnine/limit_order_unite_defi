f320462b80aec321fbf530a25d9d83b3aec220ea
royalnine <17271567+royalnine@users.noreply.github.com>
2025-08-01 12:34:13 +0200

    only metamask

--- frontend/lib/wagmi.ts
+++ frontend/lib/wagmi.ts
@@ -19,7 +19,7 @@ const localhost = {
 export const config = createConfig({
   chains: [localhost, base],
   connectors: [
-    injected(),
+    injected({ target: 'metaMask' }),
   ],
   transports: {
     [localhost.id]: http(),
